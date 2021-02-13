0041a8e3c2e382434e0b3fff70ce9ad702b0ec2d
AnvarHadja <74791981+AnvarHadja@users.noreply.github.com>
2021-02-13 00:54:01 -0800

    REST WRAPPER CREATED IN SUPPORT

--- src/test/java/support/HxroRestWrapper.java
+++ src/test/java/support/HxroRestWrapper.java
@@ -14,6 +14,90 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HxroRestWrapper {
 
+    private String baseUrl = "https://uat.hxro.io/api/";
+    private static String loginToken;
+
+    private static Map<String, Object> userCreatedInfo;
+
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String JSON = "application/json";
+    public static final String AUTH = "Authorization";
+    private static JsonPath metadata;
+
+    public static  Map<String, Object> getUserCreatedInfo() {
+        return userCreatedInfo;
+    }
+
+    public void login(Map<String, Object> credentials) {
+
+        // prepare
+
+        RequestSpecification request = RestAssured
+                .given()
+                .log().all()
+                .header(CONTENT_TYPE, JSON)
+                .body(credentials);
+
+        // execute
+        Response response = request
+                .post(baseUrl + "Registration/register-user/");
+
+        // verify and extract
+        Map<String, String> result = response
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .jsonPath()
+                .getMap("");
+        loginToken = "Bearer " + result.get("token");
+        // getMetadata();
+    }
+
+    public Map<String, Object> createUser(Map<String, String> userCreated) {
+
+
+        // prepare
+        RequestSpecification request = RestAssured
+                .given()
+                .log().all()
+                .header(CONTENT_TYPE, JSON)
+                .header(AUTH, loginToken)
+                .body(userCreated);
+
+        // execute
+        Response resposne = request
+                .post(baseUrl + " Account/by-user");
+
+        // verify and extract
+        Map<String, Object> result = resposne
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getMap("");
+
+        userCreatedInfo = result;
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/test/resources/features/hxro_object.feature
+++ src/test/resources/features/hxro_object.feature
@@ -3,5 +3,5 @@ Feature: Sign up flow
 
   @hxro_object1
   Scenario: Sign up
-#    Given I sign up in hxro via REST API
-#    Then I verify account info via REST API
+    Given I sign up in hxro via REST API
+    Then I verify account info via REST API
